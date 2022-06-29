61141d74efc74f68d6704978b0538fe4b1e32aad
stanford kangethe <stanfordkangethe16@gmail.com>
2022-06-29 10:04:01 +0300

    Changing the appearance and some improvements.

--- src/index.js
+++ src/index.js
@@ -1,62 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form')
-    const btn = document.querySelector('button.btns')
-    const remove = document.querySelector('button.remove')
-    const like = document.querySelector('button.like')
+const searchForm = document.querySelector('#search')
 
-    //prevents the form on the page from reloading
-    form.addEventListener('submit', handleForm)
-
-    //adds a show name to the watchlist once the add to watchlist button is clicked.
-    btn.addEventListener('click', watchList)
-
-    //removes the show from watch list
-    remove.addEventListener('click', (e) => {
-        const item = document.querySelector('li')
-        item.remove()
-    })
-
-    //changes the color of the button when clicked
-    like.addEventListener('click', (e) => {
-        like.style.backgroundColor = 'red'
-    })
-
-
-    function handleForm(e) {
-            e.preventDefault()
-            const input = document.querySelector('input#searchMovie')
-            fetch(`http://api.tvmaze.com/search/shows?q=${input.value}`)
-            .then(response => response.json())
-            .then(data => {
-                    const movie = document.querySelector('div.container')
-                    const poster = document.createElement('div')
-                    poster.innerHTML = `
-                        <img src=${data[0].show.image.medium}>
-                        <div class="details">
-                        <p>${data[0].show.name}</p>
-                        <p>${data[0].show.summary}</p>
-                        </div>
-                        <div class="buttons">
-                        <button class="towatch">Add to Watch</button>
-                        <button class="like">Like</button>
-                        </div> 
-                        `
-                    movie.appendChild(poster)
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = document.querySelector('#movie-name')
+    fetch(`http://api.tvmaze.com/search/shows?q=${input.value}`)
+      .then(resp => resp.json())
+      .then(data => {
+            const image = document.querySelector('#image-container')
+            const img = document.createElement('img')
+            img.src = data[0].show.image.medium
+            image.appendChild(img)
+            const movieName = document.querySelector('#show-name')
+            movieName.innerText = data[0].show.name
+            const summary = document.querySelector('#summary')
+            summary.innerHTML = data[0].show.summary
+            const watchlist = document.querySelector('#listWatch')
+            const addWatchlist = document.querySelector('#addWatchlist')
+            addWatchlist.addEventListener('click', () => {
+                const li = document.createElement('li')
+                li.innerHTML = `<button class="remove">X</button>${data[0].show.name}`
+                watchlist.appendChild(li)
+                const remove = document.querySelector('.remove')
+                remove.addEventListener('click', () => {
+                const li = document.querySelector('li')
+                li.remove()
+            })
+            const rated = document.querySelector('#ratedList')
+            const like = document.querySelector('#like')
+            like.addEventListener('click', () => {
+                like.style.backgroundColor = "red"
+                const li = document.createElement('li')
+                li.innerHTML = `<button class="remove">X</button>${data[0].show.name}`
+                rated.appendChild(li)
+                const remove = document.querySelector('.remove')
+                remove.addEventListener('click', () => {
+                const li = document.querySelector('li')
+                li.remove()
                 })
-    }
-
-
-    function watchList() {
-        const watchlist = document.querySelector('div.watch')
-        const item = document.createElement('li')
-        const input = document.querySelector('input#searchMovie')
-        fetch(`http://api.tvmaze.com/search/shows?q=${input.value}`)
-        .then(response => response.json())
-        .then(data => {
-            item.innerHTML = `
-                <button class="remove">X</button> ${data[0].show.name}
-                `
-        watchlist.appendChild(item)
+            })
+            })
         })
-    }
+})
+
+const showForm = document.querySelector('#show-form')
+showForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const showName = document.querySelector('.show-name')
+    const listWatch = document.querySelector('#listWatch')
+    const li = document.createElement('li')
+    li.textContent = showName.value
+    listWatch.appendChild(li)
 })
